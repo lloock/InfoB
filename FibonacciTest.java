4be9c89dbb18729a43191ec9b1363331a4e5fb00
Louis Loock <35171758+lloock@users.noreply.github.com>
2018-04-21 17:10:25 +0200

    Create FibonacciTest.java

---
+++ FibonacciTest.java
@@ -0,0 +1,12 @@
+/**
+ * This class is simply testing the print of fibonacci numbers
+ * @author Louis Loock
+ *
+ */
+public class FibonacciTest {
+
+	public static void main(String[] args) {
+		FibonacciPrint.printFibonacci(39);
+	}
+
+}
