ba968c70bb5869b3ac5aa51fbbd66e37e7f42e85
Louis Loock <35171758+lloock@users.noreply.github.com>
2018-05-07 10:45:50 +0200

    Update Aviary.java

--- Aviary.java
+++ Aviary.java
@@ -13,14 +13,14 @@ public class Aviary {
       System.out.println("3: dodo.getAbility()              : " + dodo.getAbility());
 
       Parrot parrot = new Parrot();
-      System.out.println("4: parrot.allAbilities()            : " + parrot.allAbilities());
+      System.out.println("4: parrot.allAbilities()          : " + parrot.allAbilities());
       System.out.println("5: parrot.ability                 : " + parrot.ability);
 
       Bird carsten = new Dodo();
       System.out.println("6: carsten.ability                : " + carsten.ability);
       System.out.println("7: ((Bird)carsten).allAbilities() : " + ((Bird) carsten).allAbilities());
 
-      Bird einstein = parrot;
+      Bird einstein = parrot; // = new Parrot();
       System.out.println("8: einstein.allAbilities()        : " + einstein.allAbilities());
       System.out.println("9: einstein.getAbility()          : " + einstein.getAbility());
       System.out.println("10: ((Parrot)einstein).ability    : " + ((Parrot) einstein).ability);
